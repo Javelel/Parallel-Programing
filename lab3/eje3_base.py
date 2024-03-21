d0324346a0f788095d405b2504f42f174aadf59a
Pawel <01169568@gmail.com>
2024-03-21 19:00:44 +0100

    apdate

--- lab3/eje3_base.py
+++ lab3/eje3_base.py
@@ -1,30 +1,41 @@
 import time 
 import random 
-from threading import Thread 
+from threading import Thread, Barrier, Semaphore
 
-def Hidrogeno(): 
-    #tiempo que tarda en generarse el atomo 
-    time.sleep(random.randint(1, 4)) 
-    print("H") 
+def moleculagenerada():
+	print ("Generada una nueva molécula de H20") 
+	time.sleep(0.5)
 
-def Oxigeno():  
-    #tiempo que tarda en generarse el atomo 
-    time.sleep(random.randint(1, 4)) 
-    print("O") 
+barrier = Barrier(3, action=moleculagenerada)
+semaphore_H = Semaphore(2)
+semaphore_O = Semaphore(1)
 
-def moleculagenerada(): 
-    print ("Generada una nueva molécula de H20") 
-    time.sleep(0.5) 
-   
+
+def Hidrogeno():
+	#tiempo que tarda en generarse el atomo 
+	time.sleep(random.randint(1, 4)) 
+	print("H")
+	semaphore_H.acquire()
+	barrier.wait()
+	semaphore_H.release()
+
+def Oxigeno():
+	#tiempo que tarda en generarse el atomo 
+	time.sleep(random.randint(1, 4)) 
+	print("O")
+	semaphore_O.acquire()
+	barrier.wait()
+	semaphore_O.release()
+
+
 
 if __name__ == '__main__': 
+	listahilos = []   
 
-    listahilos = []   
-
-    for i in range(10): 
-        listahilos.append(Thread(target=Oxigeno)) 
-    for j in range(10): 
-        listahilos.append(Thread(target=Hidrogeno)) 
-        
-    for t in listahilos: 
-        t.start()
+	for i in range(10): 
+		listahilos.append(Thread(target=Oxigeno)) 
+	for j in range(10): 
+		listahilos.append(Thread(target=Hidrogeno)) 
+		
+	for t in listahilos: 
+		t.start()
